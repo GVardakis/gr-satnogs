57e273bae47b06b66b090457a1869b42f432433b
George Vardakis <vardakis@csd.uoc.gr>
2017-08-16 15:30:32 +0300

    tr

--- apps/flowgraphs/satellites/satnogs_noaa_apt_decoder.py
+++ apps/flowgraphs/satellites/satnogs_noaa_apt_decoder.py
@@ -5,7 +5,7 @@
 # Title: NOAA APT Decoder
 # Author: Manolis Surligas, George Vardakis
 # Description: A NOAA APT Decoder with automatic image synchronization
-# Generated: Wed Aug 16 15:13:23 2017
+# Generated: Wed Aug 16 15:30:24 2017
 ##################################################
 
 from gnuradio import analog
@@ -67,15 +67,8 @@ class satnogs_noaa_apt_decoder(gr.top_block):
         # Blocks
         ##################################################
         self.satnogs_tcp_rigctl_msg_source_0 = satnogs.tcp_rigctl_msg_source("127.0.0.1", rigctl_port, False, 1000/doppler_correction_per_sec, 1500)
-        self.satnogs_ogg_encoder_0 = satnogs.ogg_encoder(file_path, 48000, 0.8)
         self.satnogs_noaa_apt_sink_0 = satnogs.noaa_apt_sink(decoded_data_file_path, 2080, 1500, bool(split_images), bool(sync), bool(flip_images))
         self.satnogs_coarse_doppler_correction_cc_0 = satnogs.coarse_doppler_correction_cc(rx_freq, samp_rate_rx /first_stage_decimation)
-        self.rational_resampler_xxx_1 = filter.rational_resampler_fff(
-                interpolation=48000,
-                decimation=int(samp_rate_rx/ ( first_stage_decimation  * int(samp_rate_rx/ first_stage_decimation / initial_bandwidth)) / audio_decimation),
-                taps=None,
-                fractional_bw=None,
-        )
         self.rational_resampler_xxx_0_0 = filter.rational_resampler_fff(
                 interpolation=1,
                 decimation=4,
@@ -120,7 +113,6 @@ class satnogs_noaa_apt_decoder(gr.top_block):
         ##################################################
         self.msg_connect((self.satnogs_tcp_rigctl_msg_source_0, 'freq'), (self.satnogs_coarse_doppler_correction_cc_0, 'freq'))
         self.connect((self.analog_wfm_rcv_0, 0), (self.band_pass_filter_0, 0))
-        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_1, 0))
         self.connect((self.band_pass_filter_0, 0), (self.fir_filter_xxx_1, 0))
         self.connect((self.blocks_complex_to_mag_0, 0), (self.rational_resampler_xxx_0_0, 0))
         self.connect((self.fft_filter_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
@@ -130,7 +122,6 @@ class satnogs_noaa_apt_decoder(gr.top_block):
         self.connect((self.osmosdr_source_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.hilbert_fc_0, 0))
         self.connect((self.rational_resampler_xxx_0_0, 0), (self.satnogs_noaa_apt_sink_0, 0))
-        self.connect((self.rational_resampler_xxx_1, 0), (self.satnogs_ogg_encoder_0, 0))
         self.connect((self.satnogs_coarse_doppler_correction_cc_0, 0), (self.fft_filter_xxx_0, 0))
 
     def get_antenna(self):
